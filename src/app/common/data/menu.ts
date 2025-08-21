3fd8b68cb3cf0cc589c969ee33e9996761d3f186
RJDev26 <68474302+RJDev26@users.noreply.github.com>
2025-08-21 20:20:05 +0530

    feat: add client master to side menu

--- src/app/common/data/menu.ts
+++ src/app/common/data/menu.ts
@@ -6,7 +6,8 @@ export const verticalMenuItems = [
     new Menu (215, 'Live Orders', '/live-orders', null, 'list_alt', null, false, 0),
     new Menu (216, 'Jobbing Deals', '/jobbing-deals', null, 'history', null, false, 0),
     new Menu (217, 'Manager Master', '/manager-master', null, 'supervisor_account', null, false, 0),
-    new Menu (218, 'Broker Master', '/broker-master', null, 'people', null, false, 0)
+    new Menu (218, 'Broker Master', '/broker-master', null, 'people', null, false, 0),
+    new Menu (219, 'Client Master', '/client-master', null, 'person', null, false, 0)
 ]
 
 export const horizontalMenuItems = [
@@ -15,5 +16,6 @@ export const horizontalMenuItems = [
     new Menu (215, 'Live Orders', '/live-orders', null, 'list_alt', null, false, 0),
     new Menu (216, 'Jobbing Deals', '/jobbing-deals', null, 'history', null, false, 0),
     new Menu (217, 'Manager Master', '/manager-master', null, 'supervisor_account', null, false, 0),
-    new Menu (218, 'Broker Master', '/broker-master', null, 'people', null, false, 0)
+    new Menu (218, 'Broker Master', '/broker-master', null, 'people', null, false, 0),
+    new Menu (219, 'Client Master', '/client-master', null, 'person', null, false, 0)
 ]
